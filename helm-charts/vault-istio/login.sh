56c2a1d1de10e2dfdc935957d23a395c4ffa7c9b
Canpolat Oral <canpoalotral@gmail.com>
2022-08-01 23:34:26 +0400

    c

--- helm-charts/vault-istio/login.sh
+++ helm-charts/vault-istio/login.sh
@@ -1,5 +1,6 @@
 # export VAULT_ADDR=http://192.168.99.109:30752
 export VAULT_ADDR='http://127.0.0.1:8200'
+export VAULT_SKIP_VERIFY=1
 export VAULT_TKN=$(cat cluster-keys.json | jq -r ".root_token") 
 export VAULT_TOKEN=root
 
